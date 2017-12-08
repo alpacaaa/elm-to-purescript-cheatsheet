fe137017d9d45ccf83ba702365a16fe44837a3ba
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-12-08 12:46:11 +0100

    It works!

--- elm-architecture/buttons/Buttons.purs
+++ elm-architecture/buttons/Buttons.purs
@@ -3,33 +3,39 @@ module Buttons.Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
 import Data.Const (Const)
 import Data.Monoid (mempty)
-import Spork.Html (Html, div, text)
+import Spork.Html (Html, div, text, button, onClick)
 import Spork.App (BasicApp)
 import Spork.App as App
 import Spork.Interpreter as Interpreter
 
-type Model =
-    Unit
+type Model = Int
 
-data Msg
+data Msg = Increment | Decrement
 
 render :: Model -> Html Msg
 render model =
-    div [] [text "YOLO"]
+    div []
+        [ button [ onClick (const $ Just Decrement) ] [ text "-" ]
+        , div [] [ text (show model) ]
+        , button [ onClick (const $ Just Increment) ] [ text "+" ]
+        ]
 
 
 update :: Model -> Msg -> App.Transition (Const Void) Model Msg
 update model msg =
-    App.purely model
+    case msg of
+        Increment -> App.purely (model + 1)
+        Decrement -> App.purely (model - 1)
 
 app :: BasicApp (Const Void) Model Msg
 app =
     { render
     , update
     , subs: const mempty
-    , init: App.purely unit
+    , init: App.purely 0
     }
 
 -- This is probably the wrong way of going about this
