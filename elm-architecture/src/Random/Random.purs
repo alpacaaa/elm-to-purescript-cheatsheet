376ccc5c3a3b0e926eccb8d654a0836abf4500fe
Ebmtranceboy <ebmtranceboy@gmail.com>
2019-12-04 20:25:47 +0100

    adapted to purs 0.13.5

--- elm-architecture/src/Random/Random.purs
+++ elm-architecture/src/Random/Random.purs
@@ -2,45 +2,37 @@ module Random.Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Random as Random
-import Data.Maybe
-import Data.Monoid (mempty)
+import Effect.Random as Random
+import Effect(Effect)
+import Data.Maybe(Maybe(..))
 import Data.Const (Const)
 import Spork.App as App
-import Spork.Html (Html, div, button, onClick, text, h1)
+import Spork.Html (Html, div, button, onClick, text, h1) as H
 import Spork.Interpreter (liftNat, merge, never)
-
-
 
 type Model =
     { dieFace :: Int
     }
 
-
 data Msg
     = Roll
     | NewFace Int
 
-
-data Effect a
+data Unpure a
     = GenerateRandom (Int -> a)
 
-
-init :: App.Transition Effect Model Msg
+init :: App.Transition Unpure Model Msg
 init =
     App.purely { dieFace: 1 }
 
-
-render :: Model -> Html Msg
+render :: Model -> H.Html Msg
 render model =
-    div []
-        [ h1 [] [ text (show model.dieFace) ]
-        , button [ onClick (const $ Just Roll) ] [ text "Roll" ]
+    H.div []
+        [ H.h1 [] [ H.text (show model.dieFace) ]
+        , H.button [ H.onClick (const $ Just Roll) ] [ H.text "Roll" ]
         ]
 
-
-update :: Model -> Msg -> App.Transition Effect Model Msg
+update :: Model -> Msg -> App.Transition Unpure Model Msg
 update model msg =
     case msg of
         Roll ->
@@ -49,8 +41,7 @@ update model msg =
         NewFace newFace ->
             App.purely $ model { dieFace = newFace }
 
-
-app :: App.App Effect (Const Void) Model Msg
+app :: App.App Unpure (Const Void) Model Msg
 app =
     { render
     , update
@@ -58,16 +49,14 @@ app =
     , init
     }
 
-
-runEffect :: forall eff. Effect ~> Eff _
+runEffect ::  forall a. Unpure a -> Effect a
 runEffect effect =
     case effect of
         GenerateRandom next -> do
             n <- Random.randomInt 1 6
             pure (next n)
 
-
-main :: Eff _ Unit
+main :: Effect Unit
 main = do
     inst <- App.makeWithSelector (liftNat runEffect `merge` never) app "#app"
     inst.run
